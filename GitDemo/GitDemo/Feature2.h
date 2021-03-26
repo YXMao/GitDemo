d47179e572abda327dc979cc472f6ef355bee4c5
YXMao <295911401@qq.com>
2021-03-26 15:29:20 +0800

    功能点2

---
+++ GitDemo/GitDemo/Feature2.h
@@ -0,0 +1,16 @@
+//
+//  Feature2.h
+//  GitDemo
+//
+//  Created by maoyuxiang on 2021/3/26.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+//功能点2
+@interface Feature2 : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GitDemo/GitDemo/Feature2.m
@@ -0,0 +1,12 @@
+//
+//  Feature2.m
+//  GitDemo
+//
+//  Created by maoyuxiang on 2021/3/26.
+//
+
+#import "Feature2.h"
+
+@implementation Feature2
+
+@end
